6a6943f49ebb348000db55c6ab22092446c1ce0f
zen <0differenthang@gmail.com>
2025-12-16 22:16:41 +0700

    refactor code

--- src/components/tablePlayer.jsx
+++ src/components/tablePlayer.jsx
@@ -6,45 +6,45 @@ export default function TablePlayer() {
   const [player, setPlayer] = useState([]);
 
   useEffect(() => {
-    tampilPlayer().then(setPlayer).catch(console.error);
+    tampilPlayer()
+      .then(setPlayer)
+      .catch(console.error);
   }, []);
 
   return (
     <>
       <div className="container">
-       <div class="card">
-  <div class="table-responsive text-nowrap">
-    <table class="table">
-     <thead>
-        <tr>
-          <th>NO</th>
-          <th>NIS</th>
-          <th>Nama</th>
-          <th>Jenis</th>
-          <th>Bulan</th>
-          <th>Jumlah</th>
-        </tr>
-      </thead>
-          <tbody>
-            {player.map((a, index) => (
-              <tr key={a.id}>
-                <td className="px-4">{index + 1}</td>
-                <td>
-                  <img src={a.photo} alt={a.name} width="50" className="rounded-circle" />
-                </td>
-                <td>{a.name}</td>
-                <td>{a.position}</td>
-                <td>{a.teams}</td>
-                <td>
-                <button className="btn btn-success mx-3">Edit</button>
-                <button className="btn" style={{backgroundColor: "#992820", color: "white"}}>Delete</button>
-                </td>
+        <div className="table-responsive text-nowrap">
+          <table className="table mt-3 align-middle">
+            <thead>
+              <tr>
+                <th>NO</th>
+                <th>Photo</th>
+                <th>Name</th>
+                <th>Position</th>
+                <th>Teams</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      </div>
+            </thead>
+            <tbody className="table-border-bottom-0">
+              {player.map((a, index) => (
+                <tr key={a.id}>
+                  <td className="px-4">{index + 1}</td>
+                  <td>
+                    <img src={a.photo} alt={a.name} width="50" className="rounded-circle" />
+                  </td>
+                  <td>{a.name}</td>
+                  <td>{a.position}</td>
+                  <td>{a.teams}</td>
+                  <td>
+                    <button className="btn btn-success mx-3">Edit</button>
+                    <button className="btn" style={{ backgroundColor: "#992820", color: "white" }}>Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
